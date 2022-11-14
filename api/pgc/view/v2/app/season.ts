a6b00f92e2a974f7d0f9529464293c2cba315c5b
xrz <3545270592@qq.com>
2022-11-14 20:22:46 +0800

    Update season.ts

--- api/pgc/view/v2/app/season.ts
+++ api/pgc/view/v2/app/season.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch'
-import * as env from '../../../_config'
+import * as env from '../../../../_config'
 
 const api = env.api_playurl
 
