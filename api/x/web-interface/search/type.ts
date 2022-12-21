bf78259c528ca0d263a17e07c3def6b8f70a6293
xrz-cloud <62951481+xrz-cloud@users.noreply.github.com>
2022-12-21 12:19:16 +0800

    支持web版搜索

--- api/x/web-interface/search/type.ts
+++ api/x/web-interface/search/type.ts
@@ -1,19 +1,23 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import fetch from 'node-fetch'
-import * as env from '../../../_config'
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import fetch from "node-fetch";
+import * as env from "../../../_config";
+import { getCookies } from "../../../utils/_bili"
 
-const api = env.api_playurl
-
-//TODO 需要cookie认证
+const api = env.api_playurl;
 
 const main = async (req: VercelRequest, res: VercelResponse) => {
-  fetch(api + req.url, {
+  const cookies = await getCookies()
+  await fetch(api + req.url, {
     method: req.method,
-    body: req.body
-  }).then(response => response.json())
-    .then(response => {
-      res.json(response)
-    })
-}
+    body: req.body,
+    headers: {
+      cookie: cookies,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      res.json(response);
+    });
+};
 
-export default main
+export default main;
