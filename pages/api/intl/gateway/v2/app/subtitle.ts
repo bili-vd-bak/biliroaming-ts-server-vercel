72f7f64d0d0c1457ea4b5294cbfb391d963fddcc
xrz-cloud <62951481+xrz-cloud@users.noreply.github.com>
2022-12-24 13:54:58 +0800

    intl支持搜索;intl剧集详情部分完成;intl获取playurl被B站风控

---
+++ pages/api/intl/gateway/v2/app/subtitle.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as env from "../../../../_config";
+// import * as data_parse from "./_data";
+
+const main = async (req: NextApiRequest, res: NextApiResponse) => {
+  /* const continue_execute = await data_parse.middleware(
+    req.url as string,
+    req.headers
+  );
+  if (continue_execute[0] == false) res.json(env.block(continue_execute[1]));
+  else res.json(await data_parse.main(req.url as string)); */
+  fetch(env.api.intl.subtitle + req.url, {
+    method: req.method,
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      res.json(response);
+    });
+};
+
+export default main;
