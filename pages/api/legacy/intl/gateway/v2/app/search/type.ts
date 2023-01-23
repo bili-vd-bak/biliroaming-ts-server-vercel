0194dfc29b2880882a2663e6a96adbd064f9cba1
xrz-cloud <62951481+xrz-cloud@users.noreply.github.com>
2023-01-23 19:37:29 +0800

    准备支持grpc下的playurl,subtitle功能

---
+++ pages/api/legacy/intl/gateway/v2/app/search/type.ts
@@ -0,0 +1,86 @@
+// import type { VercelRequest, VercelResponse } from '@vercel/node';
+import type { NextApiRequest, NextApiResponse } from "next";
+import fetch from "node-fetch";
+import * as env from "../../../../../../_config";
+
+const api = env.api.intl.search;
+const basic_res = {
+  area: "漫游",
+  badge: "公告",
+  badges: [
+    {
+      bg_color: "#00C0FF",
+      bg_color_night: "#0B91BE",
+      bg_style: 1,
+      text: env.fs_badges,
+      text_color: "#FFFFFF",
+      text_color_night: "#E5E5E5",
+    },
+  ],
+  badges_v2: [
+    {
+      bg_color: "#FF6699",
+      bg_color_night: "#D44E7D",
+      bg_style: 1,
+      text: env.fs_badges,
+      text_color: "#FFFFFF",
+      text_color_night: "#FFFFFF",
+    },
+  ],
+  cover: env.fs_cover,
+  cv: "",
+  episodes: [
+    { index: "1", param: "1", position: 1, uri: env.fs_watch_button_link },
+  ],
+  episodes_new: new Function("return" + JSON.stringify(env.fs_episodes_app))(),
+  follow_button: {
+    icon: "http://i0.hdslb.com/bfs/bangumi/154b6898d2b2c20c21ccef9e41fcf809b518ebb4.png",
+    status_report: "bangumi",
+    texts: {
+      "0": env.fs_follow_button_title,
+      "1": env.fs_unfollow_button_title,
+    },
+  },
+  goto: "bangumi",
+  is_atten: 0,
+  is_selection: 1,
+  label: env.fs_label,
+  media_type: 1,
+  param: "1",
+  ptime: 1500000000,
+  pub_time: "",
+  rating: env.fs_rating,
+  season_id: 1,
+  season_type: 1,
+  season_type_name: "番剧",
+  selection_style: env.fs_selection_style,
+  staff: "无",
+  style: env.fs_style,
+  styles: env.fs_style,
+  styles_v2: env.fs_style,
+  title: env.fs_title,
+  uri: env.fs_uri,
+  vote: env.fs_vote,
+  watch_button: {
+    link: env.fs_watch_button_link,
+    title: env.fs_watch_button_title,
+  },
+};
+
+// const main = async (req: VercelRequest, res: VercelResponse) => {
+const main = async (req: NextApiRequest, res: NextApiResponse) => {
+  fetch(api + req.url, {
+    method: req.method,
+  })
+    .then((response) => response.json())
+    .then((response: { data: { items: Array<object> }; code: number }) => {
+      if (response.code === 0) {
+        let m_res = response;
+        if (m_res.data.items) m_res["data"]["items"].splice(0, 0, basic_res);
+        else m_res["data"]["items"] = [basic_res];
+        res.json(m_res);
+      } else res.json(response);
+    });
+};
+
+export default main;
---
+++ pages/api/legacy/pgc/player/api/_data.ts
@@ -0,0 +1,183 @@
+import qs from "qs";
+import fetch from "node-fetch";
+import * as env from "../../../../_config";
+import * as blacklist from "../../../../utils/notion-database/_blacklist";
+import * as db from "../../../../utils/_sstore";
+import * as db_notion from "../../../../utils/notion-database/_db";
+import * as bili from "../../../../utils/_bili";
+
+const addNewLog = async (data: {
+  access_key: string;
+  UID: number;
+  vip_type: 0 | 1 | 2;
+  url: string;
+}) => {
+  const source = await db.get("log", true);
+  if (!source) return;
+  const source_json = JSON.parse(source);
+  const log: {
+    access_key: string;
+    UID: number;
+    vip_type: 0 | 1 | 2;
+    url: string;
+    visit_time: number;
+  }[] = source_json;
+  log.push({
+    access_key: data.access_key,
+    UID: data.UID,
+    vip_type: data.vip_type,
+    url: data.url,
+    visit_time: Date.now(),
+  });
+  return db.set("log", JSON.stringify(log));
+};
+
+const addNewLog_notion = async (data: any) => {
+  const res = await db_notion.update(
+    env.NOTION_db_log,
+    JSON.stringify({
+      parent: { database_id: env.NOTION_db_log },
+      properties: {
+        access_key: {
+          id: "title",
+          type: "title",
+          title: [
+            {
+              type: "text",
+              text: { content: data.access_key },
+              plain_text: data.access_key,
+            },
+          ],
+        },
+        UID: {
+          number: data.UID,
+        },
+        vip_type: {
+          number: data.vip_type,
+        },
+        url: {
+          rich_text: [
+            { type: "text", text: { content: data.url }, plain_text: data.url },
+          ],
+        },
+        visit_time: {
+          number: Date.now(),
+        },
+      },
+    })
+  );
+  return res;
+};
+
+const readCache = async (
+  cid: number,
+  ep_id: number,
+  info: { uid: number; vip_type: 0 | 1 | 2 }
+) => {
+  const c_vip = await db.get(`c-vip-${cid}-${ep_id}`);
+  if (c_vip) {
+    if (
+      env.whitelist_vip_enabled &&
+      (await blacklist.main(info.uid).data?.is_whitelist)
+    )
+      return c_vip;
+    if (info.vip_type !== 0) return await db.get(`c-vip-${cid}-${ep_id}`);
+  } else return await db.get(`c-${cid}-${ep_id}`);
+};
+
+const addNewCache = async (url_data: string, res_data) => {
+  const need_vip = res_data.has_paid ? 1 : 0;
+  const url = new URL(url_data, env.api.main.app.playurl);
+  const data = qs.parse(url.search.slice(1));
+
+  if (need_vip)
+    db.set(
+      `c-vip-${Number(data.cid)}-${Number(data.ep_id)}`,
+      JSON.stringify(res_data),
+      Date.now() + env.cache_time
+    );
+  else
+    db.set(
+      `c-${Number(data.cid)}-${Number(data.ep_id)}`,
+      JSON.stringify(res_data),
+      Date.now() + env.cache_time
+    );
+};
+
+const fetchDataFromBiliAndCache = async (url_data: string) => {
+  const res = (await fetch(env.api.main.app.playurl + url_data).then((res) =>
+    res.json()
+  )) as { code: number };
+  if (res.code === 0) await addNewCache(url_data, res);
+  return res;
+};
+
+/**
+ * 数据处理中间件 \
+ * 返回为 true - 继续执行 \
+ * 返回为 false - 阻止进行 \
+ * 返回为 true 时，0-标准执行 1-检查vip
+ * 返回一个 [boolean,number] 参数0决定是否继续执行，参数1决定封锁信息
+ * @param url_data 域名后的请求数据
+ * @param headers 请求头
+ * @return {boolean} boolean
+ */
+export const middleware = async (
+  url_data: string,
+  headers
+): Promise<[boolean, number]> => {
+  //请求头验证
+  if (!headers["x-from-biliroaming"]) return [false, 1];
+  if (env.ver_min != 0 && env.ver_min >= Number(headers["build"]))
+    return [false, 2];
+  //信息获取
+  const url = new URL(url_data, env.api.main.app.playurl);
+  if (!url.search || !url.search) return [false, 7]; //缺少参数
+  //免登陆
+  const info = await bili.access_keyParams2info(url.search);
+  if (info.uid === 0) {
+    //查询信息失败
+    if (!env.need_login) return [true, 0];
+    else return [false, 6];
+  }
+  //信息获取
+  const data = qs.parse(url.search.slice(1));
+  if (!data.access_key) return [false, 7]; //缺少参数 need_login=1才需此行
+  if (env.need_login && !data.access_key) return [false, 6]; //need_login强制为1
+  await addNewLog({
+    access_key: data.access_key as string,
+    UID: info.uid,
+    vip_type: info.vip_type,
+    url: url_data,
+  });
+  if (env.NOTION_db_log)
+    await addNewLog_notion({
+      access_key: data.access_key as string,
+      UID: info.uid,
+      vip_type: info.vip_type,
+      url: url_data,
+    });
+  //黑白名单验证
+  const blacklist_data = await blacklist.main(info.uid);
+  if (blacklist_data.code != 0) return [false, 3];
+  else {
+    if (env.whitelist_enabled) {
+      if (blacklist_data.data.is_whitelist) return [true, 0];
+      else return [false, 5];
+    }
+    if (env.blacklist_enabled && blacklist_data.data.is_blacklist)
+      return [false, 4];
+    return [true, 0];
+  }
+};
+
+export const main = async (url_data: string) => {
+  //信息获取
+  const url = new URL(url_data, env.api.main.app.playurl);
+  const data = qs.parse(url.search.slice(1));
+  const info = await bili.access_keyParams2info(url.search);
+  if (env.need_login && info.uid === 0) return env.block(6);
+  const rCache = await readCache(Number(data.cid), Number(data.ep_id), info);
+  if (rCache) return JSON.parse(rCache);
+  else return fetchDataFromBiliAndCache(url_data);
+};
