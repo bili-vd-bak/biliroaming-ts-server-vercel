9de56c2028d3c1ed6d3256dc1d6233b870a87d82
xrz-cloud <62951481+xrz-cloud@users.noreply.github.com>
2023-12-31 22:03:11 +0800

    bump to 3.1.4

--- src/_config.ts
+++ src/_config.ts
@@ -294,7 +294,7 @@ export const block = (code: number) => {
 //============================================================
 
 //===================信息展示(不用改)===========================
-export const version = `3.1.2[${
+export const version = `3.1.4[${
   (
     process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ||
     process.env.VERCEL_GIT_COMMIT_SHA
